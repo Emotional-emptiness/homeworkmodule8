a0917a93da55b2541fd2f0cd0138f67351efcb9a
Serg <moon.linexx@gmail.com>
2023-07-09 11:49:57 +0300

    8 module homework ( added new 3 classes )

--- src/GraphicsEditor.java
+++ src/GraphicsEditor.java
@@ -2,8 +2,15 @@ public class GraphicsEditor {
     public static void main(String[] args) {
         Shape circle = new Circle(5.0);
         Shape quad = new Quad(10.0);
+        Shape triangle = new Triangle(8.0, 4.0);
+        Shape rectangle = new Rectangle(6.0, 3.0);
+        Shape square = new Square(7.0);
 
         ShapePrinter.printShapeName(circle);
         ShapePrinter.printShapeName(quad);
+        ShapePrinter.printShapeName(triangle);
+        ShapePrinter.printShapeName(rectangle);
+        ShapePrinter.printShapeName(square);
+
     }
 }
